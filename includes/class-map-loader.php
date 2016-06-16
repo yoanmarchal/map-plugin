c8bbafe121dff5e4836bc0f247d550e100460852
Marchal yoan <marchalyoan@gmail.com>
2016-06-16 15:02:29 -0400

    Applied fixes from StyleCI

--- includes/class-map-loader.php
+++ includes/class-map-loader.php
@@ -52,10 +52,10 @@ class MapPluginLoader
      *
      * @since    1.0.0
      *
-     * @param string $hook          The name of the WordPress action that is being registered.
-     * @param object $component     A reference to the instance of the object on which the action is defined.
-     * @param string $callback      The name of the function definition on the $component.
-     * @param int    $priority      Optional. he priority at which the function should be fired. Default is 10.
+     * @param string $hook         The name of the WordPress action that is being registered.
+     * @param object $component    A reference to the instance of the object on which the action is defined.
+     * @param string $callback     The name of the function definition on the $component.
+     * @param int    $priority     Optional. he priority at which the function should be fired. Default is 10.
      * @param int    $acceptedArgs Optional. The number of arguments that should be passed to the $callback. Default is 1.
      */
     public function addAction($hook, $component, $callback, $priority = 10, $acceptedArgs = 1)
@@ -68,10 +68,10 @@ class MapPluginLoader
      *
      * @since    1.0.0
      *
-     * @param string $hook          The name of the WordPress filter that is being registered.
-     * @param object $component     A reference to the instance of the object on which the filter is defined.
-     * @param string $callback      The name of the function definition on the $component.
-     * @param int    $priority      Optional. he priority at which the function should be fired. Default is 10.
+     * @param string $hook         The name of the WordPress filter that is being registered.
+     * @param object $component    A reference to the instance of the object on which the filter is defined.
+     * @param string $callback     The name of the function definition on the $component.
+     * @param int    $priority     Optional. he priority at which the function should be fired. Default is 10.
      * @param int    $acceptedArgs Optional. The number of arguments that should be passed to the $callback. Default is 1
      */
     public function addFilter($hook, $component, $callback, $priority = 10, $acceptedArgs = 1)
@@ -85,11 +85,11 @@ class MapPluginLoader
      *
      * @since    1.0.0
      *
-     * @param array  $hooks         The collection of hooks that is being registered (that is, actions or filters).
-     * @param string $hook          The name of the WordPress filter that is being registered.
-     * @param object $component     A reference to the instance of the object on which the filter is defined.
-     * @param string $callback      The name of the function definition on the $component.
-     * @param int    $priority      The priority at which the function should be fired.
+     * @param array  $hooks        The collection of hooks that is being registered (that is, actions or filters).
+     * @param string $hook         The name of the WordPress filter that is being registered.
+     * @param object $component    A reference to the instance of the object on which the filter is defined.
+     * @param string $callback     The name of the function definition on the $component.
+     * @param int    $priority     The priority at which the function should be fired.
      * @param int    $acceptedArgs The number of arguments that should be passed to the $callback.
      *
      * @return array The collection of actions and filters registered with WordPress.
